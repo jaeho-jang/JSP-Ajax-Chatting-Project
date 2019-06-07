d026ed08430b2deb6f76364efb2c234257139a03
Jaeho Jang <jaehojang@nate.com>
2019-06-07 15:10:44 +0900

    feat: Implement member registration function

---
+++ UserChat/src/user/UserDAO.java
@@ -0,0 +1,117 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class UserDAO {
+
+	DataSource dataSource;
+
+	public UserDAO() {
+		try {
+			InitialContext initContext = new InitialContext(); // WAS와 연결하는 부분(연결 컨텍스트 형성)
+			Context envContext = (Context) initContext.lookup("java:/comp/env"); // 연결된 WAS에서 데이터 소스 검색해서 가져오기(JNDI 활용)
+			dataSource = (DataSource) envContext.lookup("jdbc/UserChat");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// 로그인 시도
+	public int login(String userID, String userPassword) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE userID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if (rs.getString(userPassword).equals(userPassword)) {
+					return 1; // 로그인 성공
+				}
+				return 2; // 패스워드 오류
+			} else {
+				return 0; // 해당 사용자 존재하지 않음
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; // DB Error
+	}
+	
+	// ID 중복 체크
+	public int register(String userID, String userPassword, String userName, String userAge, String userGender, String userEmail, String userProfile) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String SQL = "INSERT INTO USER VALUES (?, ?, ?, ?, ?, ?, ?)";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			pstmt.setString(2, userPassword);
+			pstmt.setString(3, userName);
+			pstmt.setInt(4, Integer.parseInt(userAge));
+			pstmt.setString(5, userGender);
+			pstmt.setString(6, userEmail);
+			pstmt.setString(7, userProfile);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; // DB Error
+	}
+	
+	// 회원가입 시도
+	public int registerCheck(String userID) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String SQL = "SELECT * FROM USER WHERE userID = ?";
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if (rs.next() || userID.equals("")) {
+				return 0; // 이미 존재하는 회원이거나 입력하지 않음
+			} else {
+				return 1; // 가입 가능한 회원 ID
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return -1; // DB Error
+	}
+	
+}
