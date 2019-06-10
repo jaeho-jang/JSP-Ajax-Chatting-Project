d39c16e9c19e28ab5aa82d67f55aa8e16884f8ac
Jaeho Jang <jaehojang@nate.com>
2019-06-10 17:02:50 +0900

    feat: Implement message read processing

--- UserChat/src/chat/ChatListServlet.java
+++ UserChat/src/chat/ChatListServlet.java
@@ -38,7 +38,7 @@ public class ChatListServlet extends HttpServlet {
 		StringBuffer result = new StringBuffer("");
 		result.append("{\"result\":[");
 		ChatDAO dao = new ChatDAO();
-		ArrayList<ChatDTO> chatList = dao.getChatListByRecent(fromID, toID, 10);
+		ArrayList<ChatDTO> chatList = dao.getChatListByRecent(fromID, toID, 100);
 		if (chatList.size() == 0) return "";
 		for (int i = 0; i < chatList.size(); i++) {
 			result.append("[{\"value\": \"" + chatList.get(i).getFromID() + "\"},");
@@ -48,6 +48,7 @@ public class ChatListServlet extends HttpServlet {
 			if (i != chatList.size() - 1) result.append(",");
 		}
 		result.append("], \"last\":\"" + chatList.get(chatList.size() - 1).getChatID() + "\"}");
+		dao.readChat(fromID, toID);
 		return result.toString();
 	}
 	
@@ -65,6 +66,7 @@ public class ChatListServlet extends HttpServlet {
 			if (i != chatList.size() - 1) result.append(",");
 		}
 		result.append("], \"last\":\"" + chatList.get(chatList.size() - 1).getChatID() + "\"}");
+		dao.readChat(fromID, toID);
 		return result.toString();
 	}
 
